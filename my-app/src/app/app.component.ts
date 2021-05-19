9202cb4f7df77498bb37c7c186b991343a2abb25
dario <dario@192.168.1.19>
2021-05-19 21:51:05 +0200

    navegacion a medias, intento de background dinamico

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  oneState = false;
+  twoState = false;
+
   title = 'pruebas';
 }
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,23 +1,32 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { OneComponent } from './one/one.component';
-
-
+import { OneComponent } from './components/one/one.component';
+import { TwoComponent } from './components/two/two.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    OneComponent
-  ],
+    OneComponent,
+    TwoComponent,
+    InicioComponent
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot([
+      {path: '', component: AppComponent},
+      {path: 'one', component: OneComponent},
+      {path: 'two', component: TwoComponent}
+    ])
   ],
   exports: [AppComponent],
   providers: [],
